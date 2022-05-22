1013bba37c902a8103f019f731965b934f761bb2
irancss <buynovin94@gmail.com>
2022-05-22 21:04:00 +0430

    add  IProductRepository

---
+++ Src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+
+        #region Get 
+        Task<IEnumerable<Product>> GetProductsAsync();
+        IEnumerable<Product> GetProducts();
+
+
+        Task<Product> GetProductByIdAsync(string id);
+        Product GetProductById(string id);
+
+
+
+        Task<IEnumerable<Product>> GetProductByNameAsync(string name);
+        IEnumerable<Product> GetProductByName(string name);
+
+
+        Task<IEnumerable<Product>> GetProductByCategoryAsync(string categoryName);
+        IEnumerable<Product> GetProductByCategory(string categoryName);
+
+        #endregion
+
+
+        #region Crud
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(string id);
+
+        #endregion
+
+
+
+
+
+
+    }
+}
