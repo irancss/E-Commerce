d9f0f81a4f766a57d9f9d2e6dc941a348ebdb59d
irancss <buynovin94@gmail.com>
2022-05-22 20:55:17 +0430

    Create Context and Seed Data for Products Entity

---
+++ Src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        Products = database.GetCollection<Product>(
+            configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+
+        #region Seeding Product
+
+        CatalogContextSeed.SeedData(Products);
+
+        #endregion
+    }
+
+
+    public IMongoCollection<Product> Products { get; }
+}
