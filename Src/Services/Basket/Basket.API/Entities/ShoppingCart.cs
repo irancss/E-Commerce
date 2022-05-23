75e232d374b1a6dbf9678d1ab4898111382e1dc2
irancss <buynovin94@gmail.com>
2022-05-23 20:27:20 +0430

    Add Entities

---
+++ Src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var items in ShoppingCartItems)
+                {
+                    totalPrice += items.Price * items.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
