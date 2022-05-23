95b9b21dfab8d939fd3d71ca61969104890107ec
irancss <buynovin94@gmail.com>
2022-05-23 20:40:34 +0430

    add IBasketRepository

---
+++ Src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,23 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasketAsync(string userName);
+        ShoppingCart GetBasket(string userName);
+
+        Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket);
+        ShoppingCart UpdateBasket(ShoppingCart basket);
+
+        Task DeleteBasketAsync(string userName);
+        void DeleteBasket(string userName);
+
+    }
+
+
+    //public class BasketRepository : IBasketRepository
+    //{
+
+    //}
+}
