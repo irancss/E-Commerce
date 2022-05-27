3865bb2366f6e95357f915d417c4b4ec572591cf
irancss <buynovin94@gmail.com>
2022-05-28 01:39:15 +0430

    add mapping and get query list

---
+++ Src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersViewModel>>
+    {
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+    }
+}
--- Src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ Src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingProfile : Profile
     {
-
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersViewModel>().ReverseMap();
+        }
     }
 }
