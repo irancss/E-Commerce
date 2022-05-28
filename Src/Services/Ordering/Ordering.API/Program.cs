65dbc95a083745aac065fbca3c2381b50c53b14a
irancss <buynovin94@gmail.com>
2022-05-29 01:37:46 +0430

    add Infrastructure Service Registration

--- Src/Services/Ordering/Ordering.API/Program.cs
+++ Src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,5 @@
 using Ordering.Application;
+using Ordering.infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices();
-//builder.Services.AddInfrastructureServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Src/Services/Ordering/Ordering.infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.infrastructure.Mail;
+using Ordering.infrastructure.Persistence;
+using Ordering.infrastructure.Repositories;
+
+namespace Ordering.infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection service, IConfiguration configuration)
+        {
+
+            service.AddDbContext<OrderContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"));
+            });
+
+
+            service.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));//For mediatoR
+            service.AddScoped<IOrderRepository, OrderRepository>();
+
+            service.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            service.AddTransient<IEmailService, EmailService>();
+
+            return service;
+        }
+    }
+}
