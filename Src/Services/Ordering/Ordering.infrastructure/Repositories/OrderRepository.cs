8388f91acd00bcaaa94c1d64c63579c77eac373f
irancss <buynovin94@gmail.com>
2022-05-28 23:57:38 +0430

    Add Repository Base and Order Repository and EmailService

---
+++ Src/Services/Ordering/Ordering.infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.infrastructure.Persistence;
+
+namespace Ordering.infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _context.Orders.Where(c => c.UserName == userName).ToListAsync();
+            return orderList;
+        }
+    }
+}
