7f7ce3b5349661cccb4b6d25b7fe23a84d88ece4
irancss <buynovin94@gmail.com>
2022-05-28 19:58:18 +0430

    add service in application

---
+++ Src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection _service)
+        {
+
+            _service.AddAutoMapper(Assembly.GetExecutingAssembly());
+            _service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            _service.AddMediatR(Assembly.GetExecutingAssembly());
+
+
+            _service.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            _service.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return _service;
+
+        }
+    }
+}
