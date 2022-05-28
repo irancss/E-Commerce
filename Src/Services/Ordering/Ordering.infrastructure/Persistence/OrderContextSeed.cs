ff660807e9cc66fe0720a9a62e58f696bbd49d5e
irancss <buynovin94@gmail.com>
2022-05-28 23:16:54 +0430

    add dbContext with options and seeding Data

---
+++ Src/Services/Ordering/Ordering.infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreConfiguredOrder());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed Database successfully");
+            }
+        }
+
+
+        public static IEnumerable<Order> GetPreConfiguredOrder()
+        {
+            return new List<Order>()
+            {
+                new Order()
+                {
+                    UserName = "swn",
+                    FirstName = "amir",
+                    LastName = "varz",
+                    EmailAddress = "example@gmail.com",
+                    AddressLine = "Tehran",
+                    Country = "Iran",
+                    TotalPrice = 350
+                }
+            };
+        }
+    }
+
+
+}
