116154f9f1b4d0a1368a73a7901b704c7252ee11
irancss <buynovin94@gmail.com>
2022-05-30 00:31:47 +0430

    add migration and seeding Data

--- Src/Services/Ordering/Ordering.API/Program.cs
+++ Src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.OpenApi.Models;
+using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.infrastructure;
+using Ordering.infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,18 +11,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
+});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
+
+app.MigrateDatabase<OrderContext>((context, service) =>
+{
+    var logger = service.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1");
+    });
 }
 
 app.UseAuthorization();
