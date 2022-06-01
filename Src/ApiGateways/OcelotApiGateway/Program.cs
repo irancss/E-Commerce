fa3e9694ca79c76049461c24852fee7c60c85ce4
irancss <buynovin94@gmail.com>
2022-06-02 01:04:59 +0430

    add config in program

--- Src/ApiGateways/OcelotApiGateway/Program.cs
+++ Src/ApiGateways/OcelotApiGateway/Program.cs
@@ -3,12 +3,23 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json");
+
+
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+
+
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseOcelot().Wait();
+
 app.Run();
 
-await app.UseOcelot();
