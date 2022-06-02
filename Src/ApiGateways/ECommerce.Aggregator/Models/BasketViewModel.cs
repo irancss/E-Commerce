1467e18fe98020202277ac5530c61b6fd3607191
irancss <buynovin94@gmail.com>
2022-06-02 14:54:17 +0430

    add basket view model

---
+++ Src/ApiGateways/ECommerce.Aggregator/Models/BasketViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Aggregator.Models;
+
+public class BasketViewModel
+{
+    public string UserName { get; set; }
+    public List<BasketItemExtendedViewModel> ShoppingCartItems { get; set; } = new List<BasketItemExtendedViewModel>();
+
+    public decimal TotalPrice { get; set; }
+
+
+}
