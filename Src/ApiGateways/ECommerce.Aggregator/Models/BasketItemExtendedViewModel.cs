f9751eb593795b8f7092773704c14f01d760826d
irancss <buynovin94@gmail.com>
2022-06-02 14:57:58 +0430

    add additional fields for basket item and add order view model

--- Src/ApiGateways/ECommerce.Aggregator/Models/BasketItemExtendedViewModel.cs
+++ Src/ApiGateways/ECommerce.Aggregator/Models/BasketItemExtendedViewModel.cs
@@ -7,4 +7,13 @@ public class BasketItemExtendedViewModel
     public decimal Price { get; set; }
     public string ProductId { get; set; }
     public string ProductName { get; set; }
+
+
+    //product related additional fields
+
+    public string Category { get; set; }
+    public string Summary { get; set; }
+    public string Description { get; set; }
+    public string ImageFile { get; set; }
+
 }
---
+++ Src/ApiGateways/ECommerce.Aggregator/Models/OrderResponseViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace ECommerce.Aggregator.Models;
+
+public class OrderResponseViewModel
+{
+    public string UserName { get; set; }
+    public decimal TotalPrice { get; set; }
+
+
+    #region billing Address
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? AddressLine { get; set; }
+    public string? Country { get; set; }
+    public string? State { get; set; }
+    public string? ZipCode { get; set; }
+
+    #endregion
+
+
+    #region Payment
+    public string CardName { get; set; }
+    public string CardNumber { get; set; }
+    public string Expiration { get; set; }
+    public string CW { get; set; }
+    public string PaymentMethod { get; set; }
+
+    #endregion
+}
