e13722ffab47ae9de959e8476dcfffb709b26dd8
irancss <buynovin94@gmail.com>
2022-06-02 14:59:29 +0430

    add ECommerceViewModel

---
+++ Src/ApiGateways/ECommerce.Aggregator/Models/ECommerceViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Aggregator.Models;
+
+public class ECommerceViewModel
+{
+    public string UserName { get; set; }
+    public BasketViewModel BasketWithProducts { get; set; }
+    public IEnumerable<OrderResponseViewModel> Orders { get; set; }
+}
